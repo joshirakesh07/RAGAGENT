989fde918f825d813388f0e7849a5a991057db8d
joshirakesh07 <24r21a6622@mlrit.ac.in>
2026-08-05 11:29:18 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,7 +1,4 @@
 from langchain_core.documents import Document
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
 
 big_paragraph = (
     "The Internet is a global system of interconnected computer networks that uses the Internet protocol suite (TCP/IP) to communicate between networks and devices. "
@@ -13,24 +10,6 @@ big_paragraph = (
     "Today the Internet supports cloud computing, email, social media, video conferencing, file sharing, online education, healthcare, e-commerce and much more."
 )
 
-documents = [Document(page_content=big_paragraph)]
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=300,
-    chunk_overlap=50
-)
-
-splits = text_splitter.split_documents(documents)
-
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
-
-vector_store = FAISS.from_documents(
-    splits,
-    embeddings
-)
-
-retriever = vector_store.as_retriever(
-    search_kwargs={"k": 3}
-)
+documents = [
+    Document(page_content=big_paragraph)
+]
