1b10abf8d2cdc9846528fba4a54ae3e6883fa955
joshirakesh07 <24r21a6622@mlrit.ac.in>
2026-08-05 11:14:58 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -39,8 +39,9 @@ splits = text_splitter.split_documents(documents)
 # -----------------------------
 
 embeddings = GoogleGenerativeAIEmbeddings(
-    model="models/embedding-001",
+    model="models/text-embedding-004",
     google_api_key=os.getenv("GOOGLE_API_KEY")
+
 )
 
 # -----------------------------
