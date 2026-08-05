3125152f2ff0537f9a7bb333c776357e2fff9111
joshirakesh07 <24r21a6622@mlrit.ac.in>
2026-08-05 11:21:42 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,13 +1,7 @@
-
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
-import os
-
-# -----------------------------
-# Create Document
-# -----------------------------
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 big_paragraph = (
     "The Internet is a global system of interconnected computer networks that uses the Internet protocol suite (TCP/IP) to communicate between networks and devices. "
@@ -19,13 +13,7 @@ big_paragraph = (
     "Today the Internet supports cloud computing, email, social media, video conferencing, file sharing, online education, healthcare, e-commerce and much more."
 )
 
-documents = [
-    Document(page_content=big_paragraph)
-]
-
-# -----------------------------
-# Split Documents
-# -----------------------------
+documents = [Document(page_content=big_paragraph)]
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=300,
@@ -34,28 +22,14 @@ text_splitter = RecursiveCharacterTextSplitter(
 
 splits = text_splitter.split_documents(documents)
 
-# -----------------------------
-# Embeddings
-# -----------------------------
-
-embeddings = GoogleGenerativeAIEmbeddings(
-    model="models/text-embedding-004",
-    google_api_key=os.getenv("GOOGLE_API_KEY")
-
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
-
-# -----------------------------
-# Vector Store
-# -----------------------------
 
 vector_store = FAISS.from_documents(
     splits,
     embeddings
 )
-
-# -----------------------------
-# Retriever
-# -----------------------------
 
 retriever = vector_store.as_retriever(
     search_kwargs={"k": 3}
