eebce105c7ed0dc61ca3611b53096c63f160d673
joshirakesh07 <24r21a6622@mlrit.ac.in>
2026-08-05 11:40:22 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,48 +1,16 @@
-
 from fastapi import FastAPI
-from pydantic import BaseModel
-
-from agent import internet_agent
 
 app = FastAPI()
-
-
-class Query(BaseModel):
-    question: str
-
 
 @app.get("/")
 def home():
     return {
-        "message": "Internet History RAG Agent is Running 🚀"
+        "status": "SUCCESS",
+        "message": "Render is working!"
     }
 
-
-@app.post("/ask")
-def ask(query: Query):
-
-    response = internet_agent.invoke(
-        {
-            "messages": [
-                {
-                    "role": "user",
-                    "content": query.question
-                }
-            ]
-        }
-    )
-
-    message = response["messages"][-1]
-
-    if isinstance(message.content, list):
-        answer = ""
-        for item in message.content:
-            if item.get("type") == "text":
-                answer += item.get("text", "")
-    else:
-        answer = message.content
-
+@app.get("/hello")
+def hello():
     return {
-        "question": query.question,
-        "answer": answer
+        "message": "Hello World"
     }
